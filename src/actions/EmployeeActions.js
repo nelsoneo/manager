5addf78034f91115f5d5f2d6a5b2edf58d1577e9
nelsoneo <nelsoneo@gmail.com>
2021-05-18 22:01:28 -0300

    update types

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,4 +1,4 @@
-// import {} from '../types';
+import {} from './types';
 
 export const employeeUpdate = ({ prop, value}) => {
     return {
--- src/actions/types.js
+++ src/actions/types.js
@@ -3,3 +3,5 @@ export const PASSWORD_CHANGED = 'password_changed';
 export const LOGIN_USER_SUCESS = 'login_user_sucess';
 export const LOGIN_USER_FAIL = 'loging_user_fail';
 export const LOGIN_USER_START = 'login_user_start';
+
+export const EMPLOYEE_UPDATE = 'employee_update';
