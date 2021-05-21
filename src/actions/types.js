9f9b945ef39625a01d52144e1a709cda13f75d48
nelsoneo <nelsoneo@gmail.com>
2021-05-20 23:53:21 -0300

    more create

--- src/actions/types.js
+++ src/actions/types.js
@@ -5,3 +5,4 @@ export const LOGIN_USER_FAIL = 'loging_user_fail';
 export const LOGIN_USER_START = 'login_user_start';
 
 export const EMPLOYEE_UPDATE = 'employee_update';
+export const EMPLOYEE_CREATE = 'employee_create';
