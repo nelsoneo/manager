39353a118b2928e350b5a4a72a067600793492bb
nelsoneo <nelsoneo@gmail.com>
2021-05-20 23:57:50 -0300

    dispatch

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -11,7 +11,8 @@ export const employeeUpdate = ({ prop, value}) => {
 
 export const employeeCreate = ({ name, phone, shift }) => {
     const { currentUser } = firebase.auth();
-    return  () => {
+
+    return  (dispatch) => {
         firebase.database().ref(`/users/${currentUser.uid}/employees`)
         .push({ name, phone, shift })
         .then(() => Actions.employeeList({ type: 'reset'}));
