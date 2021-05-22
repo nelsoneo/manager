1d9441d0c47b22d4f1a38bbe05af9c2e076030ad
nelsoneo <nelsoneo@gmail.com>
2021-05-22 09:34:43 -0300

    end more

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,41 +1,44 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { ListView, View, Text } from 'react-native';
+import { FlatList, View, Text } from 'react-native';
 import { employeeFetch } from '../actions';
 
 class EmployeeList extends Component {
     componentDidMount(){
         this.props.employeeFetch();
 
-        this.createDataSource(this.props);
+        // this.createDataSource(this.props);
     }
 
-    componentWillUnmount(nextProps) {
-        this.createDataSource(nextProps);
-    }
+    // componentWillUnmount(nextProps) {
+    //     this.createDataSource(nextProps);
+    // }
 
-    createDataSource({ employees }) {
-        const ds = new ListView.DataSource({
-            rowHasChanged: (r1, r2) => r1 !== r2
-        });
+    // createDataSource({ employees }) {
+    //     const ds = new FlatList.DataSource({
+    //         rowHasChanged: (r1, r2) => r1 !== r2
+    //     });
 
-        this.DataSource = ds.cloneWithRows(this.props.employees);
-    }
+    //     this.DataSource = ds.cloneWithRows(this.props.employees);
+    // }
 
     render(){
+        console.log(this.props);
         return (
-            <View>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-            </View>
+            <FlatList 
+            data={this.props.employees}
+            renderItem={({ item }) => <Text>{item.name}</Text>}
+            />
         );
     }
 }
 
 const mapStateToProps = state => {
-    
+    const employees = _.map(state.employees, (val, uid) => {
+    return { ...val, key: uid };
+    });
+    return { employees };
 };
 
-export default connect(null, { employeeFetch }) (EmployeeList);
+export default connect(mapStateToProps, { employeeFetch })(EmployeeList);
